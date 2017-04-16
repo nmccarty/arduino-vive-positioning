8f6428b8147640e2a8aed67d0b78d70064f15706
Nathan McCarty <nathan.mccarty@uky.edu>
2017-04-16 01:29:09 -0400

    Pulse width detection

--- main-vectortag/main-vectortag.ino
+++ main-vectortag/main-vectortag.ino
@@ -3,22 +3,26 @@
 #include <CurieTime.h>
 
 // Used in determining noise floor
-float stdTolerance = 1.5;
+float stdTolerance = 2;
+int minPulseLength = 10;
+
+bool spiked[6];
+int noiseMeans[6];
+int noiseStds[6];
+long lastSpike[6];
 
 void setup() {
   digitalWrite(13, 1);
-  Serial.begin(9600);
+  Serial.begin(2000000);
   while(!Serial);
   digitalWrite(13, 0);
 
   analogReadResolution(12);
 
-  int numNoiseSamples= 200;
+  int numNoiseSamples= 100;
 
   Serial.println("Measuring noise floors");
   int* noiseSamples[6];
-  int noiseMeans[6];
-  int noiseStds[6];
   for(int i=0; i < 6; i++){
     noiseSamples[i] = measureNoise(i, numNoiseSamples);
     noiseMeans[i] = mean(noiseSamples[i], numNoiseSamples);
@@ -29,13 +33,35 @@ void setup() {
     Serial.print(noiseMeans[i]);
     Serial.print(" : ");
     Serial.println(noiseStds[i]);
+    free(noiseSamples[i]);
   }
+  Serial.flush();
+  for(int i=0; i<6; i++){
+    spiked[i] = false;
+  }
+
+  delay(2000);
 
   
 }
 
 void loop() {
-
+  while(true){
+    bool spike = detectSpike(0,noiseMeans[0],noiseStds[0]);
+    if(!spiked[0] && spike){
+      lastSpike[0]=micros();
+      spiked[0] = true;
+    }
+    if(spiked[0] && !spike){
+      long s = micros();
+      spiked[0] = false;
+      Serial.println(lastSpike[0]);
+      Serial.println(s - lastSpike[0]);
+      Serial.println();
+      Serial.flush();
+    }
+}
+  
 }
 
 int* measureNoise(int pin,int size){
@@ -57,14 +83,21 @@ int mean(int* samples, int size){
 }
 
 int stdDev(int* samples, int size){
-  float m = mean(samples,size);
+  float mean = 0;
+  for(int i=0; i<size; i++){
+    mean += samples[i];
+  }
+  mean = mean/size;
   float sum = 0;
   for(int i=0;i<size;i++){
-    float tmp = samples[i] - m;
+    float tmp = samples[i] - mean;
     tmp = tmp * tmp;
+    sum += tmp;
   }
   sum = sum / size;
-  return (int) sqrt(sum);
+  sum = sqrt(sum);
+  Serial.println(sum);
+  return (int) sum;
   
 }
 
@@ -72,4 +105,5 @@ bool detectSpike(int pin, int noise, int std){
   int value = analogRead(pin);
   return value > (noise + std * stdTolerance);
 }
+
 
